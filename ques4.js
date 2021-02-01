2389bad0192d96a8413ddc2304a11110c4cc886a
Preeti-Pundir <64474544+Preeti-Pundir@users.noreply.github.com>
2021-01-31 22:00:19 -1100

    Add files via upload

---
+++ ques4.js
@@ -0,0 +1,11 @@
+last =  function(array, n) {
+    if (array == null) 
+      return void 0;
+    if (n == null) 
+       return array[array.length - 1];
+    return array.slice(Math.max(array.length - n, 0));  
+    };
+  ​
+  document.write(last([7, 9, 0, -3]));
+  document.write(last([7, 9, 0, -2],3));
+  document.write(last([7, 9, 0, -2],6));
